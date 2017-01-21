b5fd441146de229eb9b6c6a97ddc13a9247ca527
tklem <tklem2@illinois.edu>
2017-01-21 10:04:09 -0500

    Changed Mongo variable

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 const config = {
-  mongoURL: (process.env.NODE_ENV === 'development' && 'mongodb://localhost:27017/mern-starter') ||
-  			(process.env.NODE_ENV === 'production' && 'mongodb://heroku_7g1wzzsv:7j5c23b314n15he49ek86fagj9@ds117869.mlab.com:17869/heroku_7g1wzzsv'),
+  mongoURL: 'mongodb://heroku_7g1wzzsv:7j5c23b314n15he49ek86fagj9@ds117869.mlab.com:17869/heroku_7g1wzzsv',
   port: process.env.PORT || 8000,
 };
 
